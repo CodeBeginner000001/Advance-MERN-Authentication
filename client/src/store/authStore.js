f353448c430fc0fd98ea241aa9c7854bf2dc61a6
Adarsh <ashu2100ag@gmail.com>
2025-01-04 23:34:34 +0530

    done changes in MongooseURL

--- client/src/store/authStore.js
+++ client/src/store/authStore.js
@@ -2,7 +2,7 @@
 // global state management
 import {create} from "zustand"
 import axios from "axios"
-const API_URL = "http://localhost:5001/api/auth";
+const API_URL = "https://advance-mern-authentication-server.onrender.com/api/auth";
 
 axios.defaults.withCredentials = true;
 export const useAuthStore = create((set)=>({
