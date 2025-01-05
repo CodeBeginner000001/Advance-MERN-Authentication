814c6255c6f3469f3b89f4ff4de24db6f19c48ea
Adarsh <ashu2100ag@gmail.com>
2025-01-05 18:38:27 +0530

    solving errors

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -92,7 +92,7 @@ export default function App() {
           }
           />
           <Route
-          path="/reset-password/:token"
+          path="https://advance-mern-authentication-jcuz.onrender.com/#/reset-password/:token"
           element={
             <RedirectAuthenticatedUser>
               <ResetPasswordPage />
