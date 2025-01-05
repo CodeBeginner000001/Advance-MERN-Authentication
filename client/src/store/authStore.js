0d8a58520d6b53c66f0102172959444b94fd38ad
Adarsh <ashu2100ag@gmail.com>
2025-01-05 20:12:44 +0530

    added alerts using react toastify

--- client/src/store/authStore.js
+++ client/src/store/authStore.js
@@ -2,7 +2,7 @@
 // global state management
 import {create} from "zustand"
 import axios from "axios"
-const API_URL = "https://advance-mern-authentication.onrender.com/api/auth";
+const API_URL = "https://advance-mern-authentication-server.onrender.com/api/auth";
 
 axios.defaults.withCredentials = true;
 export const useAuthStore = create((set)=>({
