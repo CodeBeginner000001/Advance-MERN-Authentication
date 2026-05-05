33f9c1ed2dae90c05c0b8a8b9e8c61288b2450e9
Adarsh <ashu2100ag@gmail.com>
2026-05-05 22:01:52 +0530

    solving cors error

--- server/utils/generateTokenAndSetCookie.js
+++ server/utils/generateTokenAndSetCookie.js
@@ -7,7 +7,7 @@ const generateTokenAndSetCookie = (res,userID)=>{
     res.cookie("token",token,{
         httpOnly: true, // secure attacks like XSS - when an attacker injects malicious scripts into web pages viewed by users, potentially compromising their data and session.
         secure: process.env.NODE_ENV === "production", // secure cookie, only sent over HTTPS
-        sameSite: "strict", // secure attacks like CSRF - when an attacker tricks a user into performing unintended actions on a web application by exploiting the trust
+        sameSite: process.env.NODE_ENV === "production" ? "none" : "strict", // "none" required for cross-site cookies in production
         maxAge: 7 * 24 * 60 * 60 * 1000, // expires in 7 days
     })
 
